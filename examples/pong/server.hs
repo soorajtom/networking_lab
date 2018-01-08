121e5f31e7a1e36d2d4770bc6c88edafd48f3207
Piyush P Kurur <ppk@cse.iitk.ac.in>
2018-01-08 06:48:17 +0530

    pong server in Haskell

---
+++ examples/pong/server.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Network.Socket hiding (send)
+import Network.Socket.ByteString
+
+main = do
+  sock <- socket AF_INET Stream 0
+  setSocketOption sock ReuseAddr 1
+  bind sock (SockAddrInet 8001 iNADDR_ANY)
+  listen sock 2
+  mainLoop sock
+
+mainLoop :: Socket -> IO ()
+mainLoop sock = do
+  conn <- accept sock
+  worker conn
+  mainLoop sock
+
+
+worker (sock,address) = do putStrLn $ unwords ["from", show address]
+                           send sock "pong"
+                           close sock
+
