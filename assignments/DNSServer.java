e0b78fd6a647907e5c53276412621667407d2e16
Sooraj Tom <111501036@smail.iitpkd.ac.in>
2018-02-02 02:09:47 +0530

    DNS Server

---
+++ assignments/DNSServer.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.net.*;
+
+class DNSServer
+{
+   public static void main(String args[]) throws Exception
+      {
+         DatagramSocket serverSocket = new DatagramSocket(53);
+            byte[] query = new byte[1024];
+            byte[] response = new byte[1024];
+            while(true)
+               {
+                  DatagramPacket receivePacket = new DatagramPacket(query, query.length);
+                  serverSocket.receive(receivePacket);
+
+                  System.out.println("Incoming query");
+                  // for (int i =0; i< query.length; i++) {
+                  //     System.out.print("0x" + String.format("%x", query[i]) + " " );
+                  // }
+                  response = query;
+                  response[2] = (byte)0b10100000; //Response, authoritative
+
+                  int pointer = 12;
+                  String domainname = "";
+
+                  while(true)
+                  {
+                     int size = query[pointer++];
+                     for(int i = 0; i < size; i++)
+                     {
+                        domainname = domainname + (char)query[pointer++];
+                     }
+                     if(query[pointer] != 0x00)
+                        domainname = domainname + ".";
+                     else
+                        break;
+                  }
+
+                  if(domainname.equals("www.james.bond"))
+                  {
+                     response[3] = 0b00000000;  //Response code
+                     response[6] = 0;
+                     response[7] = 1;           //Answer count
+
+                     response[32] = (byte)0xc0;
+                     response[33] = 0x0c;
+                     response[34] = 0;  //Type
+                     response[35] = 1;  //Type
+                     response[36] = 0;
+                     response[37] = 1;  //Class
+
+                     response[38] = 0;
+                     response[39] = 0;
+                     response[40] = 0;
+                     response[41] = 0;  //TTL
+
+                     response[42] = 0;  //Length
+                     response[43] = 4;
+
+                     response[44] = 7;
+                     response[45] = 7;
+                     response[46] = 7;
+                     response[47] = 7;
+                  }
+                  else
+                  {
+                     response[3] = 0b00000011;  //Response code
+                  }
+
+                  InetAddress IPAddress = receivePacket.getAddress();
+                  int port = receivePacket.getPort();
+
+                  DatagramPacket sendPacket = new DatagramPacket(response, response.length, IPAddress, port);
+                  serverSocket.send(sendPacket);
+               }
+      }
+}
