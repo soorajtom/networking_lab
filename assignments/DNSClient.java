09f0fe00d56442ca43ee1aa52cb2f139ddaf203a
Sooraj Tom <111501036@smail.iitpkd.ac.in>
2018-02-02 00:41:35 +0530

    DNS response parsing complete

--- assignments/DNSClient.java
+++ assignments/DNSClient.java
@@ -1,3 +1,4 @@
+/*Author: Sooraj Tom*/
 import java.io.*;
 import java.net.*;
 
@@ -47,7 +48,7 @@ public class DNSClient {
         // No more parts
         request[pointer++] = 0;
 
-        // Type -> 1 for A or 255 for *
+        // Type -> 1 for A or 255 (-1) for *
         request[pointer++] = 0;
         request[pointer++] = -1;
 
@@ -55,10 +56,10 @@ public class DNSClient {
         request[pointer++] = 0;
         request[pointer++] = 1;
 
-        System.out.println("Sending: " + pointer + " bytes");
-        for (int i =0; i< pointer; i++) {
-            System.out.print("0x" + String.format("%x", request[i]) + " " );
-        }
+        // System.out.println("Sending: " + pointer + " bytes");
+        // for (int i =0; i< pointer; i++) {
+        //     System.out.print("0x" + String.format("%x", request[i]) + " " );
+        // }
 
         // *** Send DNS Request Frame ***
         DatagramSocket socket = new DatagramSocket();
@@ -70,8 +71,77 @@ public class DNSClient {
         DatagramPacket packet = new DatagramPacket(response, response.length);
         socket.receive(packet);
 
-        System.out.println("\n\nReceived: " + packet.getLength() + " bytes");
+        // System.out.println("\n\nReceived: " + packet.getLength() + " bytes");
 
         int rescode = (int)(response[3] & 0b00001111);
 
+        switch(rescode)
+        {
+            case 0:
+                System.out.println("DNS Record Found.");
+                parseresponse(response);
+                break;
+            case 1:System.out.println("Formal Error");
+                break;
+            case 2:System.out.println("Server Failure");
+                break;
+            case 3:System.out.println("Name Error");
+                break;
+            case 4:System.out.println("Error: Not Implemented");
+                break;
+            case 5:System.out.println("Error: Refused");
+                break;
+            case 6:System.out.println("Error: YX Domain");
+                break;
+            case 7:System.out.println("Error: YX RR Set");
+                break;
+            case 8:System.out.println("Error: NX RR Set");
+                break;
+            case 9:System.out.println("Error: Not Auth");
+                break;
+            case 10:System.out.println("Error: Not Zone");
+                break;
+        }
+    }
+
+    public static void parseresponse(byte[] response)
+    {
+        int no_res = ((int)response[6]) * 512 + ((int)response[7]);
+        // System.out.println(no_res);
+
+        int pointer;
+
+        for (int i = 12; ;i++)
+        {
+            if(response[i] == 0)
+            {
+                pointer = i + 5;
+                break;
+            }
+        }
+
+        // for(int i = 0; i < response.length; i++)
+        //     System.out.print(" | " + String.format("%8s", Integer.toBinaryString(response[i] & 0xFF)) + " " );
+
+        for(int i=0; i<no_res; i++)
+        {
+            int type =  ((int) response[pointer + 2]) * 512 + ((int) response[pointer + 3]);
+            // System.out.print("Type: ");
+            // System.out.println(type);
+            int len = ((int) response[pointer + 10]) * 512 + ((int) response[pointer + 11]);
+            // System.out.println(len);
+            if(type == 1)
+            {
+                System.out.print("Address: ");
+                System.out.print((int)(response[pointer + 12] & 0xFF));
+                System.out.print(".");
+                System.out.print((int)(response[pointer + 13] & 0xFF));
+                System.out.print(".");
+                System.out.print((int)(response[pointer + 14] & 0xFF));
+                System.out.print(".");
+                System.out.println((int)(response[pointer + 15] & 0xFF));
+            }
+            pointer = pointer + 11 + len + 1;
+        }
+    }
 }
